fc617b07688bbcf722b6645a718aa5ec6dd25604
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-04-30 14:58:29 +0100

    made tts audio stop when user exits activity

--- lib/note_detail_page.dart
+++ lib/note_detail_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
-class NoteDetailPage extends StatelessWidget {
+class NoteDetailPage extends StatefulWidget {
   final String note;
   final DateTime timestamp;
-  final FlutterTts flutterTts = FlutterTts();
 
   NoteDetailPage({
     Key? key,
     required this.note,
     required this.timestamp,
   }) : super(key: key);
+
+  @override
+  _NoteDetailPageState createState() => _NoteDetailPageState();
+}
+
+class _NoteDetailPageState extends State<NoteDetailPage> {
+  final FlutterTts flutterTts = FlutterTts();
+
+  @override
+  void dispose() {
+    // Stop speaking when the page is disposed
+    flutterTts.stop();
+    super.dispose();
+  }
 
   // Function to format the date and time to display only hours and minutes
   String formatDateTime(DateTime dateTime) {
@@ -40,33 +53,34 @@ class NoteDetailPage extends StatelessWidget {
               'Note:',
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            Text(note),
+            Text(widget.note),
             SizedBox(height: 16.0),
             Text(
               'Timestamp:',
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            Text(formatDateTime(timestamp)),
+            Text(formatDateTime(widget.timestamp)),
             SizedBox(height: 16.0),
             ElevatedButton(
-                onPressed: () {
-                  _speakNote();
-                },
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('Read Aloud'),
-                    SizedBox(width: 15), // Adjust spacing between text and icon
-                    SizedBox(
-                      width: 24, // Adjust the width as needed
-                      height: 24, // Adjust the height as needed
-                      child: Opacity(
-                        opacity: 0.7, // Set opacity to 70%
-                        child: Image.asset('assets/images/marketing.png'),
-                      ),
+              onPressed: () {
+                _speakNote();
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Read Aloud'),
+                  SizedBox(width: 15), // Adjust spacing between text and icon
+                  SizedBox(
+                    width: 24, // Adjust the width as needed
+                    height: 24, // Adjust the height as needed
+                    child: Opacity(
+                      opacity: 0.7, // Set opacity to 70%
+                      child: Image.asset('assets/images/marketing.png'),
                     ),
-                  ],
-                )),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -74,6 +88,6 @@ class NoteDetailPage extends StatelessWidget {
   }
 
   Future<void> _speakNote() async {
-    await flutterTts.speak(note);
+    await flutterTts.speak(widget.note);
   }
 }
